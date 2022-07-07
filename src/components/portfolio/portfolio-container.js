09f69ab0de07608d678ad701d92e94fa0d35c6ee
Sephyyr <joserodluis@gmail.com>
2022-07-06 19:16:58 -0600

    update

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 
+import PortfolioItem from "./portfolio-item";
+
 export default class PortfolioContainer extends Component {
+  constructor() {
+    super();
+
+    console.log("Portfolio container has rendered");
+  }
+
   render() {
     return (
       <div>
-        <h3>Portfolio items go here updated...</h3>
+        <h2>Portfolio items go here updated...</h2>
+
+        <PortfolioItem />
       </div>
     );
   }
