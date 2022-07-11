bb94cebfb706f9758b198a66b16f4e00b84d655a
Sephyyr <joserodluis@gmail.com>
2022-07-11 16:53:50 -0600

    update

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>My, Jose RodCas React Starter</h1>
+      <div className="app">
+        <NavigationContainer />
+        <h1>Jordan Hudgens Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
-        <h2>My First React Application</h2>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
       </div>
     );
   }
 }
-
